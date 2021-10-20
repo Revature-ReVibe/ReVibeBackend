22541cd0f9ae07e89ec03e0e445766ad49384dd4
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-10-20 11:36:07 -0700

    registration unit tests

---
+++ src/test/java/com/ReVibe/ReVibe/accountTest/TestAccountService.java
@@ -0,0 +1,50 @@
+package com.ReVibe.accountTest;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.ReVibe.model.Account;
+import com.ReVibe.repository.AccountRepository;
+import com.ReVibe.service.AccountService;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestAccountService {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@InjectMocks
+	AccountService accountService;
+	
+	@Mock
+	AccountRepository accountRepository;
+	
+	
+	@BeforeAll
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(accountService).build();
+	}
+	
+	@Test
+	public void testSave() {
+		Account account = new Account();
+		accountService.saveAccount(account);
+		verify(accountRepository, times(1)).save(account);
+	}
+}
