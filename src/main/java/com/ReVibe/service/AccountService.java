3ddfc9bad70a278b731ee2fdc917574506cfb86e
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-10-20 11:34:47 -0700

    registration

--- src/main/java/com/ReVibe/service/AccountService.java
+++ src/main/java/com/ReVibe/service/AccountService.java
@@ -1,5 +1,21 @@
 package com.ReVibe.service;
 
-public class AccountService {
+import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ReVibe.model.Account;
+import com.ReVibe.repository.AccountRepository;
+
+@Transactional
+@Service
+public class AccountService {
+	
+	@Autowired
+	private AccountRepository accountRepository;
+
+	public Account saveAccount(Account account) {
+		return this.accountRepository.saveAccount(account);
+	}
 }
