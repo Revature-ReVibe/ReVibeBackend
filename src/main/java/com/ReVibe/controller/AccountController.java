fda4305c134636fbd06e999efad01216aacf8b14
ewileyrev <ethan.wiley@revature.net>
2021-10-27 09:28:26 -0400

    fixed stuff

--- src/main/java/com/ReVibe/controller/AccountController.java
+++ src/main/java/com/ReVibe/controller/AccountController.java
@@ -3,10 +3,6 @@ package com.ReVibe.controller;
 
 import java.util.List;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -14,23 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ReVibe.model.Account;
 import com.ReVibe.service.AccountService;
 import com.ReVibe.service.JwtService;
-
-import io.jsonwebtoken.Claims;
 
 @RestController("accountController")
 @RequestMapping("/account")
@@ -114,33 +105,13 @@ public class AccountController {
 	
 	@PostMapping(path = "/new", consumes = MediaType.APPLICATION_JSON_VALUE) 
 	public Account saveAccount(@RequestBody Account account) {
-		return this.accountService.saveAccount(account);
-	}
-	
-
-	@GetMapping("/test")
-	public ResponseEntity<String> test() {
-		String moon = "hit the test endpoint";
-		System.out.println(moon);
-		return new ResponseEntity<String>(moon, HttpStatus.OK);
-	}
-	
-	
-	@PostMapping(path = "/signIn", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> signIn(@RequestBody Account account, HttpServletRequest request) {
-
-		account = this.accountService.findByUsernameAndPassword(account.getUsername(), account.getPassword());
-
-		if (account == null) {
-			return new ResponseEntity<String>("Incorrect user or password", HttpStatus.BAD_REQUEST);
-		} else {
-
-			HttpSession session = request.getSession();
-			session.setAttribute("userId", account.getUserId());
-			session.setAttribute("user", account);
-			System.out.println((Integer) session.getAttribute("userId"));
-			return new ResponseEntity<String>("Signed in", HttpStatus.OK);
+		try {
+		account= this.accountService.saveAccount(account);
+		}catch(org.springframework.dao.DataIntegrityViolationException e) {
+			return null;
 		}
+		return account;
 	}
+	
 }
 
