377f01ad641c9ecf6945875508f319b6b7d293e2
ayankosky <alexander.yankosky@revature.net>
2021-10-31 21:40:53 -0400

    missing feature files

--- src/test/java/com/ReVibe/stepDefinition/LogInFeature.java
+++ src/test/java/com/ReVibe/stepDefinition/LogInFeature.java
@@ -1,43 +1,93 @@
 package com.ReVibe.stepDefinition;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.ReVibe.pom.LogInPOM;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.testng.CucumberOptions;
 
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions
 public class LogInFeature {
+	private WebDriver driver;
+	private LogInPOM loginpage;
+
+	@Before
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://localhost:4200/login");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		loginpage = new LogInPOM(driver);
+	}
+
+	@After
+	public void teardown() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+
+	}
+	
 	@Given("A user is on the login page")
 	public void a_user_is_on_the_login_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		
+		assertEquals("Frontend", driver.getTitle());
 	}
-
+	
 	@When("A user enters valid {string} and {string}")
-	public void a_user_enters_valid_and(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void a_user_enters_valid_and(String username, String password) {
+		username = "firstUser";
+		password = "root";
+		loginpage.setUsername(username);
+		loginpage.setPassword(password);
+		loginpage.clickLogin();
 	}
-
+	
+	
 	@Then("A user is redirected to the {string}")
-	public void a_user_is_redirected_to_the(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void a_user_is_redirected_to_the(String currentUrl) {
+		currentUrl = "http://localhost:4200/";
+		assertEquals(currentUrl, driver.getCurrentUrl());
 	}
-
+	
 	@Given("A user is currently on the login page")
 	public void a_user_is_currently_on_the_login_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		assertEquals("Frontend", driver.getTitle());
 	}
-
+	
 	@When("A user enters a invalid {string} and {string}")
-	public void a_user_enters_a_invalid_and(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void a_user_enters_a_invalid_and(String username, String password) {
+		username = "first";
+		password = "root";
 	}
-
+	
 	@Then("A user is still on the {string}")
 	public void a_user_is_still_on_the(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		assertEquals("Frontend", driver.getTitle());
 	}
 }
--- src/test/java/com/ReVibe/stepDefinition/LogOutFeature.java
+++ src/test/java/com/ReVibe/stepDefinition/LogOutFeature.java
@@ -1,10 +1,45 @@
 package com.ReVibe.stepDefinition;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.ReVibe.pom.LogInPOM;
+
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LogOutFeature {
+	
+	private WebDriver driver;
+	private LogInPOM loginpage;
+	
+	@BeforeAll
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://localhost:4200/login");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		loginpage = new LogInPOM(driver);
+	}
+
+	@After
+	public void teardown() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+
+	}
+	
 	@Given("A user is on any {string} but login or registration")
 	public void a_user_is_on_any_but_login_or_registration(String string) {
 	    // Write code here that turns the phrase above into concrete actions
