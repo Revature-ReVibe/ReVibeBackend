a86faf3d06750ede24ddba987524b5d376579049
Pat Downton <pjdownton5017@gmail.com>
2021-11-02 10:52:06 -0500

    Repository layer documentation

--- src/main/java/com/ReVibe/repository/LikeRepository.java
+++ src/main/java/com/ReVibe/repository/LikeRepository.java
@@ -7,7 +7,26 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Integer>{
+    /**
+     * This method finds all Likes with the desired userId. 
+     * @param userId    the integer representing the id of an Account object
+     * @return          the list of Like objects with the requested userId
+     */
     public List<Like> findByUserId(int userId);
+    
+    /**
+     * This method finds all Likes with the desired vibeId.
+     * @param vibeId    the integer representing the id of a Vibe object 
+     * @return          the list of Like objects with the requested vibeId
+     */
     public List<Like> findByVibeId(int vibeId);
+    
+    /**
+     * This method finds the Like with the desired vibeId and userId.
+     * @param vibeId    the integer representing the id of a Vibe object
+     * @param userId    the integer representing the id of an Account object
+     * @return          the Like object that meets the requested criteria;
+     *                  <code>null</code> otherwise.
+     */
     public Like findByVibeIdAndUserId(int vibeId, int userId);
 }//LikeRepository
--- src/main/java/com/ReVibe/repository/VibeRepository.java
+++ src/main/java/com/ReVibe/repository/VibeRepository.java
@@ -7,8 +7,27 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VibeRepository extends JpaRepository<Vibe, Integer>{
+    /**
+     * This method finds all Vibes that are replies to different, specific Vibe
+     * @param parentVibe    the integer representing the id of the parent Vibe
+     * @return              the list of Vibe objects with the requested 
+     *                      parent Vibe;
+     *                      <code>null</code> otherwise.
+     */
     List<Vibe> findByParentVibe(int parentVibe);
+    
+    /**
+     * This method finds all Vibes posted by a specific Account
+     * @param accountId     the integer representing the id of the Account
+     * @return              the list of Vibe objects with the requested Account id
+     */
     List<Vibe> findByAccountid(int accountId);
+    
+    /**
+     * This method finds all Vibes with no parent Vibe
+     * It is used to distinguish original Vibe posts from Vibe replies.
+     * @return  the list of Vibe objects with no parent Vibe
+     */
     List<Vibe> findByParentVibeNull();
 
-}
+}//VibeRepository
